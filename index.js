f260c2f6c485d1034e6b847f10634cfabb66d259
ajaxjson <ajaxjson@gmail.com>
2022-01-07 12:53:38 +0700

    9TH commit

--- index.js
+++ index.js
@@ -14,14 +14,30 @@ mongodb.MongoClient.connect(process.env.CONNECTIONSTRING,{useUnifiedTopology:tru
     //FOR QUERY    
     // const results = await db.collection('pets').find({species:'cat'}).toArray() //return Json
     // console.log(results)
+
     //FOR INSERT
     // const pets = db.collection('pets')
     // await pets.insertOne({name:'Hugo',species:'cat',age:4},(err, data) => {
     //     if(err)  console.log(err);
-    //     console.log('saved to db: ');
+    //     console.log('saved completed');
     // })
 
+    //FOR UPDATE
+    // const pets = db.collection('pets')
+    // await pets.updateOne({_id:mongodb.ObjectId('61d712041d45754573c9908f')},{$set:{name:'Always hungry',age:5}},(err, data) => {
+    //     if(err)  console.log(err);
+    //     console.log('update completed');
+    // })
     
+    //FOR DELETE
+    const pets = db.collection('pets')
+    await pets.deleteOne({_id:mongodb.ObjectId('61d712041d45754573c9908f')},(err, data) => {
+        if(err)  console.log(err);
+        console.log('DELETE Completed');
+    })
+    
+    
+
     client.close()
 
 
