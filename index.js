fe1b9c7ea1c27384030f33a3c0dc5e8d98d721b7
ajaxjson <ajaxjson@gmail.com>
2022-01-07 11:51:51 +0700

    commit 4th

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ dotenv.config()
 const mongodb = require('mongodb')
 
 
-mongodb.connect(process.env.CONNECTIONSTRING, async function (err,client) {
-const db = client.db()
+mongodb.MongoClient.connect(process.env.CONNECTIONSTRING, async function (err,client) {
+
+if(err) {
+            console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+       }
+       const db = client.db()       
 const results = await db.collection('pets').find().toArray() //return Json
 console.log(results)
 })
+
+// app.post('/stored', (req, res) => {
+//     console.log(req.body);
+//     db.collection('quotes').insertOne(req.body, (err, data) => {
+//         if(err) return console.log(err);
+//         res.send(('saved to db: ' + data));
+//     })
+// });
