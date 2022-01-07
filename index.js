8690d4ce2ecd84731453b942e3c9639667c28b6c
ajaxjson <ajaxjson@gmail.com>
2022-01-07 10:41:02 +0700

    2nd commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ dotenv.config()
 const mongodb = require('mongodb')
 
 
-mongodb.connect(process.env.CONNECTIONSTRING,function (err,client) {
-
+mongodb.connect(process.env.CONNECTIONSTRING, async function (err,client) {
+const db = client.db()
+const results = await db.collection('pets').find().toArray() //return Json
+console.log(results)
 })
